29e6f8b181d6121d87ec8a01d876d47b41166096
00arun00 <arunjoseph.ai@gmail.com>
2020-05-26 23:26:19 -0700

    2/1458

---
+++ py/2_add_two_numbers.py
@@ -0,0 +1,43 @@
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        """
+        Args:
+            l1: (ListNode) link list of digits
+            l2: (ListNode) link list of digits 
+
+        Returns:
+            sum_list: (ListNode) link list which has the final sum
+        ___________________________________________________________
+        Key Concepts:
+            - linked lists
+        Time Complexity:
+            -O(n)
+        Space Complexity:
+            -O(n)
+
+        """
+        # edge cases
+        if not l1:
+            return l2
+        if not l2:
+            return l1
+        # initialization
+        carry = 0
+        dummy = curr = ListNode(0)
+
+        while l1 or l2 or carry:
+            if l1:
+                carry += l1.val
+                l1 = l1.next
+            if l2:
+                carry += l2.val
+                l2 = l2.next
+            curr.next = ListNode(carry%10)
+            curr = curr.next
+            carry //= 10
+
+        return dummy.next
+
+        
+
+
